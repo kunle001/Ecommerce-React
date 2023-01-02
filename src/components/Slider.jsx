b1e91555a6b770e8f4721aa767b4de5c065e535c
Olanipekun Adekunle <adekunle.olanipekun.ko@gmail.com>
2023-01-02 23:59:49 +0100

    forth

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -6,7 +6,6 @@ const Container= styled.div`
     width:100%;
     height:100vh;
     display:flex;
-    background-color:brown;
     position:relative;
 `
 const Arrow= styled.div`
@@ -26,12 +25,88 @@ const Arrow= styled.div`
     opacity:0.3
 `
 
+const Wrapper= styled.div`
+    height:100%;
+    display:flex;
+`
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+  background-color: #${props => props.bg};
+`;
+
+const Image= styled.img`
+    height:80%;
+`
+
+const ImgContainer = styled.div`
+  width: 70%;
+  height: 100%;
+`;
+
+const InfoContainer = styled.div`
+  width: 30%;
+  height: 100%;
+  padding: 50px;
+`;
+
+const Title= styled.h1`
+    font-size: 70px;
+`
+const Desc= styled.p`
+    margin:50px 0px;
+    font-size: 20px;
+    font-weight:999;
+    letter-spacing:3px
+`
+const Button= styled.button`
+    padding:10px;
+    font-size:20px;
+    background-color:transparent;
+    cursor:pointer;
+`
+
+
 const Slider = () => {
   return (
     <Container>
         <Arrow direction="left">
             <ArrowLeftOutlined/>
         </Arrow>
+        <Wrapper>
+            <Slide bg="f5fafd">
+                <ImgContainer>
+                    <Image src='https://th.bing.com/th/id/R.20dc3df9387a9a6db0678f3e60563792?rik=vS%2b7PyQZA1N4aw&pid=ImgRaw&r=0'/>
+                </ImgContainer>
+                <InfoContainer>
+                    <Title>SUMMER SALES</Title>
+                    <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS</Desc>
+                    <Button>SHOP NOW</Button>
+                </InfoContainer>            
+            </Slide>
+            <Slide bg="fcf1ed">
+                <ImgContainer>
+                    <Image src='https://th.bing.com/th/id/R.20dc3df9387a9a6db0678f3e60563792?rik=vS%2b7PyQZA1N4aw&pid=ImgRaw&r=0'/>
+                </ImgContainer>
+                <InfoContainer>
+                    <Title>WINTER SALES</Title>
+                    <Desc>Winter Wears at the Cheapest Rate</Desc>
+                    <Button>SHOP NOW</Button>
+                </InfoContainer>            
+            </Slide>
+            <Slide bg="fbf0f4">
+                <ImgContainer>
+                    <Image src='https://th.bing.com/th/id/R.20dc3df9387a9a6db0678f3e60563792?rik=vS%2b7PyQZA1N4aw&pid=ImgRaw&r=0'/>
+                </ImgContainer>
+                <InfoContainer>
+                    <Title>POPULAR SALES</Title>
+                    <Desc>Hot Sales Don't miss out</Desc>
+                    <Button>SHOP NOW</Button>
+                </InfoContainer>            
+            </Slide>
+        </Wrapper>
         <Arrow direction="right">
             <ArrowRightOutlined/>
         </Arrow>
