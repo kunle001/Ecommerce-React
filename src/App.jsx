8c37f45417a20c5a646967354aadc880db46ed49
Olanipekun Adekunle <adekunle.olanipekun.ko@gmail.com>
2023-01-05 23:52:55 +0100

    new pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 // import { Home } from "@material-ui/icons";
 // import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
 
 const App = () => {
-  return <Home/>;
+  return <Register/>;
 };
 
 export default App;
