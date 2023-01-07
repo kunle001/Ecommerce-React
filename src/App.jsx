892192119c7e79d2c366f39ea03ca14d2ffa5005
Olanipekun Adekunle <adekunle.olanipekun.ko@gmail.com>
2023-01-07 23:22:38 +0100

    mobile view added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-// import { Home } from "@material-ui/icons";
-// import Navbar from "./components/Navbar";
-// import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 import Cart from "./pages/Cart";
-// import Login from "./pages/Login";
-// import Product from "./pages/Product";
-// import ProductList from "./pages/ProductList";
-// import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
 
 const App = () => {
-  return <Cart/>;
+  return <ProductList/>;
 };
 
 export default App;
