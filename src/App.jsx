6b89a3ad9af837651b8bd55c8fee4ca600ff9474
Olanipekun Adekunle <adekunle.olanipekun.ko@gmail.com>
2023-01-08 21:29:06 +0100

    version 1 complete

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 
 const App = () => {
-  return <ProductList/>;
+  return <Home/>;
 };
 
 export default App;
