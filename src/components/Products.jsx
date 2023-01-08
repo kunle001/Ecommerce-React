4fc4a8179bdc6ef43f3dcfd07c92dd962fc0ded6
Olanipekun Adekunle <adekunle.olanipekun.ko@gmail.com>
2023-01-08 21:31:09 +0100

    product component

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,7 +4,7 @@ import {popularProducts} from '../data'
 import Product from './Product';
 
 const Container=styled.div`
-    padding:20px;
+    padding:50px;
     display:flex;
     flex-wrap: wrap;
     justify-content: space-between;
