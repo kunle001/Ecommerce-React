d6e3353e910eb58248f7cb8a26d38969913022ac
Olanipekun Adekunle <adekunle.olanipekun.ko@gmail.com>
2023-01-08 21:32:45 +0100

    product mobile updated

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -4,11 +4,13 @@ import Announcements from "../components/Announcement"
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
 import NewsLetter from "../components/NewsLetter"
+import { mobile } from "../responsive"
 
 const Container= styled.div``
 const Wrapper= styled.div`
     padding:50px;
     display:flex;
+    ${mobile({flexDirection:"column"})}
 `
 const ImgContainer= styled.div`
     flex:1
@@ -17,10 +19,13 @@ const Image= styled.img`
     width:100%;
     height:90vh;
     object-fit:cover;
+    z-index:2;
+    ${mobile({height:"50%"})}
 `
 const InfoContainer= styled.div`
     flex:1;
     padding:0px 50px;
+    ${mobile({padding: "2px"})}
     `
 const Title= styled.h1`
     font-weight:900
@@ -32,6 +37,7 @@ const Desc= styled.p`
 const Price= styled.span`
     font-weight:100;
     font-size:40px;
+    ${mobile({color:"green"})}
 `
 
 const FilterContainer= styled.div`
@@ -39,6 +45,7 @@ const FilterContainer= styled.div`
     justify-content:space-between;
     width:50%;
     margin:30px 0px;
+    ${mobile({width:"100%"})}
 
 `
 const FilterColor= styled.div`
@@ -68,6 +75,7 @@ const AddContainer= styled.div`
     width:50%;
     align-items:center;
     justify-content:space-between;
+    ${mobile({width:"100%"})}
 `;
 const AmountContainer= styled.div`
     display:flex;
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -4,6 +4,7 @@ import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
 import NewsLetter from "../components/NewsLetter"
 import Products from "../components/Products"
+import { mobile } from "../responsive"
 
 const Container= styled.div``
 const FilterContainer=  styled.div`
@@ -16,16 +17,20 @@ margin:20px;
 `
 const Filter= styled.div`
     margin:20px;
+    ${mobile({width: "0px 20px", display:"flex", flexDirection:"column"})}
 `
 const FilterText= styled.h3`
   font-size:20px;
   font-weight:600;
   margin-right:20px;
+  ${mobile({marginRight:"0px"})}
 `
 
 const Select= styled.select`
   padding:10px;
   margin-right:20px;
+  ${mobile({margin:"10px 0px"})}
+
 `
 const Option= styled.option``
 
